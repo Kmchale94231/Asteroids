e833f999a0c1732e99f8ea2fe70aa6cc14d81aae
Kai McHale <kaimchale@icloud.com>
2025-12-31 16:33:24 -0500

    Added asteroids and movement function

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,17 +12,38 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock() 
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    asteroid_field = AsteroidField()
+    player = Player(x=SCREEN_WIDTH // 2, y = SCREEN_HEIGHT // 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-            
-        log_state()
+        
+        dt = 0
+        dt = clock.tick(60) / 1000
+
+        updatable.update(dt)
+
         screen.fill("black")
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
 
+        log_state()
 
 if __name__ == "__main__":
     main()
+
